e89e05e2db75053b8b08007f8760b531bbe8950d
Neer393 <n.khatri393@gmail.com>
2024-09-12 15:44:46 +0530

    Made fixes

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./components/tpo/Login";
 import Register from "./components/tpo/Register";
-import Dashboard from "./components/tpo/Dashboard";
-import BranchStats from "./components/tpo/BranchStats";
 
 const router = createBrowserRouter([
     { path: "/", element: <Landing/> },
     { path: "/login", element: <Login/> },
-    { path: "/register", element: <Register/> },
-    { path: "/tpo", element: <Dashboard/> },
-    { path: '/tpo/:branch',element: <BranchStats />, }
+    { path: "/register", element: <Register/> }
 ]);
 
 function App() {
